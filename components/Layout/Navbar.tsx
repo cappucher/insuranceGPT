19dd0f7fa19ca82ebd92ace577c388f00705d060
Cameron Rampell <cameronrampell@gmail.com>
2023-06-12 21:23:40 -0700

    edited <a> to <Link>

--- components/Layout/Navbar.tsx
+++ components/Layout/Navbar.tsx
@@ -1,15 +1,16 @@
+import Link from "next/link";
 import { FC } from "react";
 
 export const Navbar: FC = () => {
   return (
     <div className="flex h-[50px] sm:h-[60px] border-b border-neutral-300 py-2 px-2 sm:px-8 items-center justify-between">
       <div className="font-bold text-3xl flex items-center">
-        <a
+        <Link
           className="ml-2 hover:opacity-50"
           href="/"
         >
           InsuranceGPT
-        </a>
+        </Link>
       </div>
     </div>
   );
