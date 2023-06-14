5fcdc33ec3514972af40a80c2fb2ac4447cb8058
Cameron Rampell <cameronrampell@gmail.com>
2023-06-13 20:26:15 -0700

    added insurance prompts and changed "system prompts" to "scenarios"

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,7 +24,6 @@ export default function Home() {
     setMessages(updatedMessages);
     setLoading(true);
 
-    console.log(localStorage.getItem("prompt"));
     const response = await fetch("/api/chat", {
       method: "POST",
       headers: {
